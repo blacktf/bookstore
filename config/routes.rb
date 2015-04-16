0c7b09d3768cb0e1e9914f5243b6354e02b2addb
Tim Black <blacktf@gmail.com>
2015-04-15 22:24:29 -0400

    Added assets and pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    get 'page/:page', :action => :index, :on => :collection
+  end
   root 'books#index'
   #get "/books" => "books#index", as: 'books'
   #get "/books/new" => "books#new", as: 'new_book'
