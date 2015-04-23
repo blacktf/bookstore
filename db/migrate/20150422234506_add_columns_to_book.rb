328c071f64df756085508a34fd36c0f32dfe0f53
Tim Black <blacktf@gmail.com>
2015-04-22 20:04:49 -0400

    Rolled back changes

--- db/migrate/20150422234506_add_columns_to_book.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToBook < ActiveRecord::Migration
-  def change
-    add_column :books, :image_file_name, :string
-    add_column :books, :genre, :string
-    add_column :books, :published_on, :datetime
-    add_column :books, :abstract, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422234506) do
+ActiveRecord::Schema.define(version: 20150402010006) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.integer  "pages"
     t.decimal  "price"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "image_file_name"
-    t.string   "genre"
-    t.datetime "published_on"
-    t.string   "abstract"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
